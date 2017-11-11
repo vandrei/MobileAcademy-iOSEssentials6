e3fcc8bea5ecf2f9c3343238720738dc97b3c6e1
Andrei Vasilescu <andrei.vasilescu@mready.net>
2017-11-11 16:45:40 +0200

    Course #4 Ending

--- Podfile
+++ Podfile
@@ -2,5 +2,7 @@ target 'weatherwake' do
   use_frameworks!
   
   pod 'Alamofire'
+  pod 'Firebase/Core'
+  pod 'Firebase/Messaging'
   
 end
--- weatherwake/AppDelegate.swift
+++ weatherwake/AppDelegate.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 import CoreData
+import Firebase
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -24,6 +26,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         } else {
             userDefaults.setValue(1, forKey: "app_open")
         }
+        
+        FirebaseApp.configure()
+        
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
         
         /* TODO: Uncomment this after creating CoreData Models
          
